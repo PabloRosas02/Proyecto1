dd6b27540e5fa2c2cf90beeabec25630f4cbb9f6
PabloRosas02 <142842035+PabloRosas02@users.noreply.github.com>
2026-05-17 16:37:54 -0700

    Revert "Simple dashboard"

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,21 +1,23 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Dashboard from '../views/Dashboard.vue'
-import ManualDetail from '../components/ManualContent.vue'
+import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes:[
+  routes: [
     {
       path: '/',
-      name: 'dashboard',
-      component: Dashboard
+      name: 'home',
+      component: HomeView,
     },
-    // Redirigir cualquier ruta desconocida al dashboard
     {
-      path: '/:pathMatch(.*)*',
-      redirect: '/'
-    }
-  ]
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/AboutView.vue'),
+    },
+  ],
 })
 
 export default router
